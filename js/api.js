c22637cb783350a4889bec6cc1f8557768392d62
LeanAranda <arandaleandro2003@gmail.com>
2024-08-02 19:29:38 -0300

    FUNCIONAAAAA

--- js/api.js
+++ js/api.js
@@ -1,27 +1,19 @@
+var currentPlaylist
+var currentPlaylistSongs
+var currentSong
+
 const start = async () => {
 
   const responsePlaylist = await fetch('https://audius-discovery-2.theblueprint.xyz/v1/playlists/trending?app_name=SpotifyClon')
   const playlist = await responsePlaylist.json()
   const firstPlaylist = playlist.data[0]
 
-  startLibrary(playlist)
-
-  const responseSongs = await fetch('https://audius-discovery-2.theblueprint.xyz/v1/playlists/'+firstPlaylist.id+'/tracks?app_name=SpotifyClon')
-  const songs = await responseSongs.json()
-  
-  startSongs(songs)
-}
-
-start()
-
-function startLibrary(playlist){
-
   let library = document.getElementById("lib");
 
   playlist.data.forEach(p => {
     library.innerHTML += `
       <div class="lib">
-        <button class="lib-element selectable">
+        <button class="lib-element selectable" onclick="changePlaylist('${p.id}')">
           <img src="${p.artwork['150x150']}" alt="">
           <span>
             <h4>${p.playlist_name}</h4>
@@ -32,22 +24,19 @@ function startLibrary(playlist){
     `
   });
 
-  const firstPlaylist = playlist.data[0]
-
   let title = document.getElementById("title");
 
     title.innerHTML = `
       <img src="${firstPlaylist.artwork['480x480']}" alt="">
       <div class="data">
         <h1>${firstPlaylist.playlist_name}</h1>
-        <p>${firstPlaylist.description}</p>
         <span> ${firstPlaylist.total_play_count} reproducciones · guardada ${firstPlaylist.favorite_count} veces</span>
       </div>
     `
-}
 
-function startSongs(songs){
-
+  const responseSongs = await fetch('https://audius-discovery-2.theblueprint.xyz/v1/playlists/'+firstPlaylist.id+'/tracks?app_name=SpotifyClon')
+  const songs = await responseSongs.json()
+  
   let songsList = document.getElementById("songsList");
 
   let index = 0
@@ -55,22 +44,20 @@ function startSongs(songs){
   songs.data.forEach( s => {
     index ++
     songsList.innerHTML += `
-      <tr class="selectable">
-        <th><h4>${index}</h4></th>
-        <th>
-          <div class="song">
-            <img src="${s.artwork['150x150']}" alt="">
-            <span>
-              <h3>${s.title}</h3>
-              <p>${s.user.name}</p>
-            </span>
-          </div>
-        </th>
-        <th><h4>${s.play_count}</h4></th>
-        <th></th>
-        <th><h4>${s.release_date}</h4></th>
-        <th><h4>${secondsToString(s.duration)}</h4></th>
-      </tr>
+
+      <button class="table-song selectable" onclick="changeSong('${s.id}')">
+        <div class="t1">${index}</div>
+        <div class="t2">
+          <img src="${s.artwork['150x150']}" alt="">
+          <span>
+            <h3>${s.title}</h3>
+            <p>${s.user.name}</p>
+          </span>
+        </div>
+        <div class="t3">${s.play_count}</div>
+        <div class="t4">${s.release_date}</div>
+        <div class="t5">${secondsToString(s.duration)}</div>
+      </button>
     `
   });
 
@@ -87,8 +74,79 @@ function startSongs(songs){
     </span>
   </div>
   <div class="player-2" id="player">
-    <audio controls>
+    <audio controls id="audio">
       <source src="https://audius-discovery-2.theblueprint.xyz/v1/tracks/${firstSong.id}/stream?app_name=SpotifyClon" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+  <div class="player-3">
+  </div>
+  `
+}
+
+start()
+
+async function changePlaylist(id){
+  let responseCurrentPlaylist = await fetch('https://audius-discovery-1.cultur3stake.com/v1/playlists/'+id+'?app_name=SpotifyClon')
+  currentPlaylist = await responseCurrentPlaylist.json()
+  currentPlaylist = currentPlaylist.data[0]
+
+  let title = document.getElementById("title");
+
+  title.innerHTML = `
+    <img src="${currentPlaylist.artwork['480x480']}" alt="">
+    <div class="data">
+      <h1>${currentPlaylist.playlist_name}</h1>
+      <span> ${currentPlaylist.total_play_count} reproducciones · guardada ${currentPlaylist.favorite_count} veces</span>
+    </div>
+  `
+  
+  let responseCurrentPlaylistSongs = await fetch('https://audius-discovery-2.theblueprint.xyz/v1/playlists/'+id+'/tracks?app_name=SpotifyClon')
+  currentPlaylistSongs = await responseCurrentPlaylistSongs.json()
+  
+  let songsList = document.getElementById("songsList");
+  songsList.innerHTML = ``
+
+  let index = 0
+
+  currentPlaylistSongs.data.forEach( s => {
+    index ++
+    songsList.innerHTML += `
+      <button class="table-song selectable" onclick="changeSong('${s.id}')">
+        <div class="t1">${index}</div>
+        <div class="t2">
+          <img src="${s.artwork['150x150']}" alt="">
+          <span>
+            <h3>${s.title}</h3>
+            <p>${s.user.name}</p>
+          </span>
+        </div>
+        <div class="t3">${s.play_count}</div>
+        <div class="t4">${s.release_date}</div>
+        <div class="t5">${secondsToString(s.duration)}</div>
+      </button>
+    `
+  });
+}
+
+async function changeSong(id){
+  let responseCurrentSong = await fetch('https://audius-discovery-1.cultur3stake.com/v1/tracks/'+id+'?app_name=SpotifyClon')
+  currentSong = await responseCurrentSong.json()
+  currentSong = currentSong.data
+
+  let player = document.getElementById("player");
+
+  player.innerHTML = `
+  <div class="player-1">
+    <img src="${currentSong.artwork['150x150']}" alt="">
+    <span>
+      <h4>${currentSong.title}</h4>
+      <p>${currentSong.user.name}</p>
+    </span>
+  </div>
+  <div class="player-2" id="player">
+    <audio controls id="audio">
+      <source src="https://audius-discovery-2.theblueprint.xyz/v1/tracks/${currentSong.id}/stream?app_name=SpotifyClon" type="audio/mpeg">
       Your browser does not support the audio element.
     </audio>
   </div>
