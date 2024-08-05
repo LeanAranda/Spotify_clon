483c3b4deea5423ca5177301f212441def6c0e12
LeanAranda <arandaleandro2003@gmail.com>
2024-08-05 17:42:23 -0300

    autoplay y cancion siguiente y anterior

--- js/api.js
+++ js/api.js
@@ -1,5 +1,6 @@
 var currentPlaylist
 var currentPlaylistSongs
+var currentPlaylistSongsAux
 var currentSong
 
 const start = async () => {
@@ -7,7 +8,7 @@ const start = async () => {
   const responsePlaylist = await fetch('https://audius-discovery-2.theblueprint.xyz/v1/playlists/trending?app_name=SpotifyClon')
   const playlist = await responsePlaylist.json()
   const firstPlaylist = playlist.data[0]
-
+  
   let library = document.getElementById("lib");
 
   playlist.data.forEach(p => {
@@ -24,67 +25,15 @@ const start = async () => {
     `
   });
 
-  let title = document.getElementById("title");
-
-    title.innerHTML = `
-      <img src="${firstPlaylist.artwork['480x480']}" alt="">
-      <div class="data">
-        <h1>${firstPlaylist.playlist_name}</h1>
-        <span> ${firstPlaylist.total_play_count} reproducciones · guardada ${firstPlaylist.favorite_count} veces</span>
-      </div>
-    `
+  changePlaylist(firstPlaylist.id)
 
   const responseSongs = await fetch('https://audius-discovery-2.theblueprint.xyz/v1/playlists/'+firstPlaylist.id+'/tracks?app_name=SpotifyClon')
   const songs = await responseSongs.json()
-  
-  let songsList = document.getElementById("songsList");
-
-  let index = 0
-
-  songs.data.forEach( s => {
-    index ++
-    songsList.innerHTML += `
-
-      <button class="table-song selectable" onclick="changeSong('${s.id}')">
-        <div class="t1">${index}</div>
-        <div class="t2">
-          <img src="${s.artwork['150x150']}" alt="">
-          <span>
-            <h3>${s.title}</h3>
-            <p>${s.user.name}</p>
-          </span>
-        </div>
-        <div class="t3">${s.play_count}</div>
-        <div class="t4">${s.release_date}</div>
-        <div class="t5">${secondsToString(s.duration)}</div>
-      </button>
-    `
-  });
 
   const firstSong = songs.data[0]
+
+  changeSong(firstSong.id, false, true)
   
-  let player = document.getElementById("player");
-
-  player.innerHTML = `
-  <div class="player-1">
-    <img src="${firstSong.artwork['150x150']}" alt="">
-    <span>
-      <h4>${firstSong.title}</h4>
-      <p>${firstSong.user.name}</p>
-    </span>
-  </div>
-  <div class="player-2" id="player">
-    <audio controls id="audio">
-      <source src="https://audius-discovery-2.theblueprint.xyz/v1/tracks/${firstSong.id}/stream?app_name=SpotifyClon" type="audio/mpeg">
-      Your browser does not support the audio element.
-    </audio>
-  </div>
-  <div class="player-3">
-  </div>
-  `
-
-  let button = document.getElementById("giantPlay");
-  button.setAttribute("onclick", "changeSong('"+firstSong.id+"')");
 }
 
 start()
@@ -115,7 +64,7 @@ async function changePlaylist(id){
   currentPlaylistSongs.data.forEach( s => {
     index ++
     songsList.innerHTML += `
-      <button class="table-song selectable" onclick="changeSong('${s.id}')">
+      <button class="table-song selectable" onclick="changeSong('${s.id}', true, true)" id="${s.id}">
         <div class="t1">${index}</div>
         <div class="t2">
           <img src="${s.artwork['150x150']}" alt="">
@@ -131,11 +80,11 @@ async function changePlaylist(id){
     `
 
     let button = document.getElementById("giantPlay");
-    button.setAttribute("onclick", "changeSong('"+currentPlaylistSongs.data[0].id+"')");
+    button.setAttribute("onclick", "changeSong('"+currentPlaylistSongs.data[0].id+"', true, true)");
   });
 }
 
-async function changeSong(id){
+async function changeSong(id, autoplay, current){
   let responseCurrentSong = await fetch('https://audius-discovery-1.cultur3stake.com/v1/tracks/'+id+'?app_name=SpotifyClon')
   currentSong = await responseCurrentSong.json()
   currentSong = currentSong.data
@@ -156,12 +105,56 @@ async function changeSong(id){
       Your browser does not support the audio element.
     </audio>
   </div>
-  <div class="player-3">
+  <div class="player-3" style="display: flex; align-items:center;">
+    <button style="color: black; background-color:white; padding:5px" onclick="changeSongLeft()">anterior</button>
+    <button style="color: black; background-color:white; padding:5px" onclick="changeSongRight()">siguiente</button>
   </div>
   `
+  if(autoplay){
+    let audio = document.getElementById("audio");
+    audio.play();
+  }
+
+  let selected = document.getElementsByClassName("selected")[0]
+  if(selected!= null){
+    selected.classList.remove("selected")
+  }
+  
+  selected = document.getElementById(id)
+  if(selected!= null){
+    selected.classList.add("selected")
+  }
+  
+  if(current){
+    currentPlaylistSongsAux = currentPlaylistSongs
+  }
 
   let audio = document.getElementById("audio");
-  audio.play();
+  audio.onended = function() {
+    changeSongRight()
+  };
+}
+
+function changeSongLeft(){
+  
+  let index = currentPlaylistSongsAux.data.findIndex(s => s.id==currentSong.id);
+  console.log(index)
+  
+  if(index != 0){
+    changeSong(currentPlaylistSongsAux.data[index-1].id, true, false)
+    console.log("cancion anterior")
+  }
+}
+
+function changeSongRight(){
+  
+  let index = currentPlaylistSongsAux.data.findIndex(s => s.id==currentSong.id);
+  console.log(index)
+  
+  if(index < currentPlaylistSongsAux.data.length){
+    changeSong(currentPlaylistSongsAux.data[index+1].id, true, false)
+    console.log("cancion siguiente")
+  }
 }
 
 function secondsToString(seconds) {
